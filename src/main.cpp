49560e517b2da34c91f59309befa8a4d018bb0f2
Stonks3141 <samuel.l.nystrom@gmail.com>
2022-03-07 18:00:04 -0500

    Fixed problems with color method in solution generator It is now very slow and also still needs to make JSON.

--- src/main.cpp
+++ src/main.cpp
@@ -6,49 +6,61 @@
 #include <sstream>
 #include <algorithm>
 
+std::string find_color(std::string guess, std::string answer)
+{
+    auto color = std::string("00000");
+    auto letters = std::map<char, int>{}; // map of already colored letters
+
+    for(auto i = 0; i < 5; ++i)
+    {
+        if(guess[i] == answer[i])
+        {
+            color[i] = '2';
+            letters[answer[i]]++;
+        }
+    }
+
+    for(auto i = 0; i < 5; ++i)
+    {
+        if(color[i] == '2')
+            continue;
+
+        auto n = std::count(answer.begin(), answer.end(), guess[i]);
+        if(n > letters[guess[i]])
+        {
+            color[i] = '1';
+            letters[guess[i]]++;
+        }
+    }
+
+    return color;
+}
+
 std::string find_best_word(std::vector<std::string> &guesses, std::vector<std::string> &answers)
 {
     auto best_guess = std::string();
     auto best_guess_count = 0;
+
     if(answers.size() <= 2)
-    {
         return answers[0];
-    }
 
     for(auto& guess : guesses)
     {
+        //std::cerr << "guessing:" << guess << " ";
         auto elim_ctr = 0.0;
 
-        auto colors = std::map<std::vector<int>, int>();
+        auto colors = std::map<std::string, int>();
 
         for(auto& answer : answers)
-        {
-            auto color = std::vector{0, 0, 0, 0, 0};
-            for(auto i = 0; i < 5; ++i)
-            {
-                if(guess[i] == answer[i])
-                    color[i] = 2;
-                else if(answer.find_first_of(guess[i]) != std::string::npos)
-                    color[i] = 1;
-            }
-            colors[color]++;
-        }
+            colors[find_color(guess, answer)]++;
 
         for(auto& [color, count] : colors)
         {
             auto elims = 0;
             for(auto& answer : answers)
             {
-                for(auto i = 0; i < 5; ++i)
-                {
-                    if((color[i] == 2 && answer[i] != guess[i]) ||
-                            (color[i] == 1 && answer.find_first_of(guess[i]) == std::string::npos) ||
-                            (color[i] == 0 && answer.find_first_of(guess[i]) != std::string::npos))
-                    {
-                        elims++;
-                        break;
-                    }
-                }
+                if(find_color(guess, answer) != color)
+                    elims++;
             }
             elim_ctr += elims * count;
         }
@@ -73,48 +85,33 @@ std::vector<std::string> thing(std::vector<std::string> &guesses, std::vector<st
 {
     auto new_answers = answers;
 
-    auto color = std::vector<int>{0, 0, 0, 0, 0};
-    for(auto i = 0; i < 5; ++i)
-    {
-        if(guess[i] == answer[i])
-            color[i] = 2;
-        else if(answer.find_first_of(guess[i]) != std::string::npos)
-            color[i] = 1;
-    }
+    auto color = find_color(guess, answer);
 
     new_answers.erase(std::remove_if(new_answers.begin(), new_answers.end(),
                                      [color, guess](std::string answer2){
-        for(auto i = 0; i < 5; ++i)
-        {
-            if(
-                    (color[i] == 2 && answer2[i] != guess[i]) ||
-                    (color[i] == 1 && answer2.find_first_of(guess[i]) == std::string::npos) ||
-                    (color[i] == 0 && answer2.find_first_of(guess[i]) != std::string::npos)
-                    )
-            {
-                return true;
-            }
-        }
-        return false;
+        if(find_color(guess, answer2) != color)
+            return true;
+        else
+            return false;
     }), new_answers.end());
 
-    std::cout << "new size: " << new_answers.size() << "\n";
+    //std::cout << "new size: " << new_answers.size() << "\n";
 
     if(new_answers.size() == 1)
     {
-        std::cout << new_answers[0] << " ";
+        std::cerr << new_answers[0] << " ";
         return {new_answers[0]};
     }
     else
     {
         auto t = find_best_word(guesses, new_answers);
-        std::cout << t << " ";
+        std::cerr << t << " ";
         if(t == answer)
-        {
             return {t};
-        }
+
         auto r = thing(guesses, new_answers, answer, t);
         r.insert(r.begin(), t);
+
         if(r.size() > 100)
         {
             std::cout << "uhoh\n";
@@ -150,22 +147,21 @@ int main(int argc, char const *argv[])
     {
         guesses.push_back(str);
     }
+    ansfile.close();
+    guessfile.close();
 
     auto speed = std::map<int, int>();
-    /*
+
     for(auto& ans : answers)
     {
-        std::cout << "roate ";
+        std::cerr << "roate ";
         auto a = thing(guesses, answers, ans, "roate");
         std::cout << "\n";
         speed[a.size()]++;
     }
-    */
-    auto a = thing(guesses, answers, "heath", "roate");
+
     for(auto& [num, ct] : speed)
-    {
         std::cout << ct << " Wordles solved in " << num << " tries\n";
-    }
 
     return 0;
 }
